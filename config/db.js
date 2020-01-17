91ac08e9a77017909da02c0eb60abdb30ad3189f
Tianhang Hu <tianhang.hu@alphayee.com>
2020-01-17 19:28:30 +0800

    修改使用数据库

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const CONNECTION_URL = 'mongodb://u-notification:u-notification@cluster0-shard-00-00-ntqve.azure.mongodb.net:27017,cluster0-shard-00-01-ntqve.azure.mongodb.net:27017,cluster0-shard-00-02-ntqve.azure.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 const DBTables = {
-    NOTE: 't_notes',
+    NOTE: 'notes',
     ACCOUNTING: 't_accounting',
     TOKEN: 't_token',
     USER: 't_user'
