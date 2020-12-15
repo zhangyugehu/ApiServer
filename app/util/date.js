0757861684d25f190c3cc602aa110ff45eabc915
hutianhang <tianhang.hu@alphayee.com>
2020-12-15 11:46:43 +0800

    时区问题

--- app/util/date.js
+++ app/util/date.js
@@ -1,16 +1,16 @@
 function extensions() {
   Date.prototype.format = function(fmt) { 
     const o = { 
-      "M+" : this.getMonth()+1,                 //月份 
-      "d+" : this.getDate(),                    //日 
-      "h+" : this.getHours(),                   //小时 
-      "m+" : this.getMinutes(),                 //分 
-      "s+" : this.getSeconds(),                 //秒 
+      "M+" : this.getUTCMonth()+1,                 //月份 
+      "d+" : this.getUTCDate(),                    //日 
+      "h+" : this.getUTCHours(),                   //小时 
+      "m+" : this.getUTCMinutes(),                 //分 
+      "s+" : this.getUTCSeconds(),                 //秒 
       "q+" : Math.floor((this.getMonth()+3)/3), //季度 
       "S"  : this.getMilliseconds()             //毫秒 
     }; 
     if(/(y+)/.test(fmt)) {
-            fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+            fmt=fmt.replace(RegExp.$1, (this.getUTCFullYear()+"").substr(4 - RegExp.$1.length)); 
     }
     for(let k in o) {
       if(new RegExp("("+ k +")").test(fmt)){
